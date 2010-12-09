c5795c4de9f7bea68d28109568ec9cd8acb2fa7c
Brian Lamar <brian.lamar@rackspace.com>
2010-12-09 17:08:04 -0500

    setup.py +x, removed egg, igonre eggs

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
+#!/usr/bin/python
+
 from setuptools import setup
 
 setup(
-		name="dhcpz",
-		version="0.1.1",
-		author="Nicholas VonHollen",
-		author_email="nicholas.vonhollen@rackspace.com",
-		license="Apache License 2.0",
-		packages=['dhcpz'],
-		package_dir={"":"src/py"},
-		data_files=[('/etc/init.d', ['src/init.d/dhcpz'])],
-		entry_points="""
-		[console_scripts]
-                dhcpz=dhcpz:main
-		"""
-		)
+    name         = "dhcpz",
+    version      = "0.1.1",
+    author       = "Nicholas VonHollen",
+    author_email = "nicholas.vonhollen@rackspace.com",
+    license      = "Apache License 2.0",
+    packages     = ['dhcpz'],
+    package_dir  = {"":"src/py"},
+    data_files   = [('/etc/init.d', ['src/init.d/dhcpz'])],
+    entry_points = """
+    [console_scripts]
+    dhcpz=dhcpz:main
+    """
+)
